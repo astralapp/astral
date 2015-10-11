368efd96ba333845931b9c9a3f5468cc6d923865
syropian <collinpatrickhenderson@gmail.com>
2015-10-11 11:34:04 -0400

    Stub out some js test stuff

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,8 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     files: [
-      'dist/main.bundle.js',
-      'assets/js/spec/**/*.spec.js'
+      'public/js/app.bundle.js',
+      'tests/js/**/*.spec.js'
     ],
     browsers: ['PhantomJS'],
     reporters: ['spec']
