9b2c56f14ab0a3b43f38bf40464b46c35df107bb
syropian <collinpatrickhenderson@gmail.com>
2015-10-11 11:26:53 -0400

    add tags stub to list items

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -24494,7 +24494,7 @@ riot.tag('star-list', '<div class="dashboard-repos"> <ul class="repos"> <star-li
 
 },{"./starListItem.tag":19,"riot":4}],19:[function(require,module,exports){
 var riot = require('riot');
-module.exports = riot.tag('star-list-item', '<li class="repo draggable" each="{new Array(10)}"> <h3 class="repo-name">astralapp/astral</h3> <div class="repo-description">Organize your GitHub stars with ease</div>  <div class="repo-stats"> <div class="repo-stat stars"><i class="fa fa-star"></i> 1337</div> <div class="repo-stat forks"><i class="fa fa-code-fork"></i> 1337</div> <div class="repo-stat link"><a href="http://github.com/astralapp/astral" target="_blank">View on GitHub</a></div> </div> </li>', function(opts) {
+module.exports = riot.tag('star-list-item', '<li class="repo draggable" each="{new Array(10)}"> <h3 class="repo-name">astralapp/astral</h3> <div class="repo-description">Organize your GitHub stars with ease</div> <ul class="repo-tags" ng-show="star.tags"> <li each="{new Array(3)}">Foo</li> </ul> <div class="repo-stats"> <div class="repo-stat stars"><i class="fa fa-star"></i> 1337</div> <div class="repo-stat forks"><i class="fa fa-code-fork"></i> 1337</div> <div class="repo-stat link"><a href="http://github.com/astralapp/astral" target="_blank">View on GitHub</a></div> </div> </li>', function(opts) {
 
 });
 
--- resources/assets/js/tags/stars/starListItem.tag
+++ resources/assets/js/tags/stars/starListItem.tag
@@ -2,9 +2,9 @@
   <li class="repo draggable" each={new Array(10)}>
     <h3 class="repo-name">astralapp/astral</h3>
     <div class="repo-description">Organize your GitHub stars with ease</div>
-    <!-- <ul class="repo-tags" ng-show="star.tags">
-      <li ng-repeat="tag in star.tags track by tag.id" ng-click="setCurrentTag(tag); $event.stopPropagation()">{{tag.name}}</li>
-    </ul> -->
+    <ul class="repo-tags" ng-show="star.tags">
+      <li each={new Array(3)}>Foo</li>
+    </ul>
     <div class="repo-stats">
       <div class="repo-stat stars"><i class="fa fa-star"></i> 1337</div>
       <div class="repo-stat forks"><i class="fa fa-code-fork"></i> 1337</div>
