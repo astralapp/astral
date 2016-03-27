f3a0e838a7df50517c2a84d23269d7a5ce7bd3b2
syropian <collinpatrickhenderson@gmail.com>
2016-03-27 11:18:37 -0400

    adding notes editor component

---
+++ resources/assets/js/components/star-notes-editor.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="repo-notes">
+    <div class="repo-note-toolbar">
+      <div class="toggle-edit-mode">
+        <input type="checkbox" id="toggle-edit-mode" v-model="previewMode" />
+        <label for="toggle-edit-mode"><i class="fa fa-eye"></i></label>
+        <div class="toggle-hint">Toggle Preview Mode</div>
+      </div>
+    </div>
+    <textarea class="repo-note-editor" @input="saveNotes | debounce 1000" v-model="currentNotes">{{ notes }}</textarea>
+    <div class="repo-note-preview" v-show="previewMode">{{{ renderedNotes }}}</div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import store from "../store/store.js";
+import marked from "marked";
+export default {
+  name: "StarNotesEditor",
+  props: ["notes"],
+  data(){
+    return {
+      currentNotes: "",
+      previewMode: false
+    }
+  },
+  computed: {
+    renderedNotes(){
+      if( this.notes && this.notes.replace(/\s/g, "") !== "" ){
+        return marked(this.notes);
+      }
+      else {
+        return "";
+      }
+    }
+  },
+  ready(){
+  },
+  methods: {
+    saveNotes(){
+      if( this.currentNotes && this.currentNotes.replace(/\s/g, "") !== "" ){
+        this.$dispatch("NOTES_SAVED", this.currentNotes);
+      }
+    }
+  }
+}
+</script>
