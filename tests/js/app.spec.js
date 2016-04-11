fb6464e9c700f482553dc24541cddfff3795973a
syropian <collinpatrickhenderson@gmail.com>
2016-04-11 14:06:51 -0400

    Commit maps into version control

--- tests/js/app.spec.js
+++
@@ -1,9 +0,0 @@
-describe("App", function () {
-  var App = require("../../resources/assets/js/components/app.vue");
-  it("should have a name", function(){
-    expect(App.name).toBe("App");
-  });
-  it("should have a router view in its template", function(){
-    expect(App.template).toMatch("<router-view></router-view>")
-  });
-})
