8b5d676741b55887eeb2c34689ca71f12715492d
syropian <collinpatrickhenderson@gmail.com>
2016-04-11 14:07:12 -0400

    Add files

---
+++ tests/js/components/dashboard.spec.js
@@ -0,0 +1,9 @@
+describe("Dashboard Component", function () {
+  var Dashboard = require("../../resources/assets/js/components/dashboard.vue");
+  it("should have a name", function(){
+    expect(Dashboard.name).toBe("Dashboard");
+  });
+  it("should have Vuex access", function(){
+    expect(Dashboard.hasOwnProperty("vuex")).toBe(true);
+  });
+})
