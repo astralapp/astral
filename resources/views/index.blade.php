721f8980ff2f066472dd5a2c75519cc51e5fce16
syropian <collinpatrickhenderson@gmail.com>
2016-04-18 19:23:22 -0400

    remove protocol from head links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,9 @@
   <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16" />
   <!-- <link rel="stylesheet" type="text/css" href="//fast.fonts.net/cssapi/055c6832-62a2-4b92-a294-454210ba5259.css"> -->
-  <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/tomorrow.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  <!-- <% HTML::style('css/prettify_themes/github.css') %>
-  <% HTML::style('bower_components/chosen_v1.1.0/chosen.min.css') %> -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   {!! Html::style('css/vendor/select2.min.css') !!}
   {!! Html::style('css/app.css') !!}
 </head>
