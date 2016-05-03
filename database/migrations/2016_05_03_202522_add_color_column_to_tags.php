43e5ba970a2f89dbcb015427c7aef6b527a70669
syropian <collinpatrickhenderson@gmail.com>
2016-05-03 16:26:12 -0400

    tag color migration file

---
+++ database/migrations/2016_05_03_202522_add_color_column_to_tags.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColorColumnToTags extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            //
+        });
+    }
+}
