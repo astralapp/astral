65972998ced47e849eb855e85f8443a0fa385ac9
syropian <collinpatrickhenderson@gmail.com>
2016-05-08 19:37:25 -0400

    User dropdown menu

--- resources/assets/js/components/user-dropdown.vue
+++ resources/assets/js/components/user-dropdown.vue
@@ -3,8 +3,8 @@
     <ul class="dashboard-userDropdownMenuItems">
       <li class="dashboard-userDropdownMenuItem"><a href="#">Settings</a></li>
       <li class="dashboard-userDropdownMenuItem"><a href="mailto:hello@astralapp.com">Support & Feedback</a></li>
-      <li class="dashboard-userDropdownMenuItem"><a href="github.com/astralapp/astral">GitHub</a></li>
-      <li class="dashboard-userDropdownMenuItem"><a href="https://www.creatorlove.com/syropian/astral"><i class="fa fa-heart"></i> Creator Love</a></li>
+      <li class="dashboard-userDropdownMenuItem"><a href="github.com/astralapp/astral" target="_blank">GitHub</a></li>
+      <li class="dashboard-userDropdownMenuItem"><a href="https://www.creatorlove.com/syropian/astral" target="_blank"><i class="fa fa-heart"></i> Creator Love</a></li>
       <li class="dashboard-userDropdownMenuItem"><a href="/logout">Sign Out</a></li>
     </ul>
   </div>
