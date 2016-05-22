f595e0f35429f9c80e9eea031a1034e2c8d9799d
Collin Henderson <syropian@users.noreply.github.com>
2016-05-22 18:08:43 -0400

    Elixir JS Builds (#49)
    * Use Laravel Elixir for JS builds
    
    * Clean up package.json
    
    * Update package.json to fix unmet peer dependencies
    
    * Update gulpfile to use elixir-vueify plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,49 +2,16 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var notify = require("gulp-notify");
-var uglify = require("gulp-uglify");
-var sourcemaps = require("gulp-sourcemaps");
-var browserify = require("browserify");
-var watchify = require("watchify");
-var babelify = require("babelify");
-var vueify = require("vueify");
-var source = require("vinyl-source-stream");
-var buffer = require("vinyl-buffer");
 var Server = require("karma").Server;
 var bourbon = require("node-bourbon");
-var assign = require("lodash/assign");
+var elixir = require("laravel-elixir");
+var vueify = require('laravel-elixir-vueify');
 
-var browserifyArgs = {
-  debug: true,
-  entries: "./resources/assets/js/app.js",
-  transform: [
-    ["babelify", {
-      extensions: [".js"]
-    }],
-    "vueify"
-  ]
-}
-
-var watchifyArgs = assign(watchify.args, browserifyArgs);
-var bundler = watchify(browserify(watchifyArgs));
-
-function scripts(){
-  console.log("Bundling started...");
-  console.time("Bundling finished");
-  return bundler
-    .bundle()
-    .on("end", function(){ console.timeEnd("Bundling finished") })
-    .pipe(source("app.bundle.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(uglify())
-    .pipe(sourcemaps.write("../maps"))
-    .pipe(gulp.dest("./public/js"))
-    .pipe(notify("JS Build Complete!"));
-}
-
-bundler.on("update", scripts);
-gulp.task("js", scripts);
+gulp.task("js", function(){
+  elixir(function(mix) {
+      mix.browserify("./resources/assets/js/app.js", "./public/js/app.bundle.js");
+  });
+});
 
 gulp.task("sass", function(){
   gulp.src("resources/assets/sass/app.scss")
