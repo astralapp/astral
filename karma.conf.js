c701140946eb3b1aa40a5f820b7abacb0be9d0dd
syropian <collinpatrickhenderson@gmail.com>
2016-05-25 14:59:41 -0400

    Writing a few sample specs for user and tag mutations

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     frameworks: ["browserify", "jasmine"],
     files: [
-      "tests/js/support/function-bind.js",
+      "tests/js/support/*.js",
       "tests/js/**/*.spec.js"
     ],
     preprocessors: {
