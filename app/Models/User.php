2e544bfbd9e4ce9faf6f50ddca55a2552466891b
syropian <collinpatrickhenderson@gmail.com>
2016-08-10 20:48:51 -0400

    fix param type in docblock

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    /** @var array */
+    /** @var string */
     protected $table = 'users';
 
     /**
