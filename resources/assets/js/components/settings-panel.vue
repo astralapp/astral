4fca2e5578aeae10d46700f568a22649696f8e3e
syropian <collinpatrickhenderson@gmail.com>
2016-08-12 18:55:00 -0400

    Starting basic settings panel layout

---
+++ resources/assets/js/components/settings-panel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="settingsPanel-backdrop">
+    <div class="settingsPanel-panel">
+      <div class="settingsPanel-header">
+        <h3>Settings</h3>
+        <button class="settingsPanel-closePanel">&times;</button>
+      </div>
+      <div class="settingsPanel-panelContent">
+        <div class="settingsPanel-row">
+          <div class="settingsPanel-settingName">
+            Auto-Tag by Language
+          </div>
+          <div class="settingsPanel-settingControl">
+            <toggle-switch :checked="languageAutotag"></toggle-switch>
+          </div>
+        </div>
+        <div class="settingsPanel-row">
+          <div class="settingsPanel-settingName">
+            Export Stars As JSON
+          </div>
+          <div class="settingsPanel-settingControl">
+            <button class="btn-flat">Export</button>
+          </div>
+        </div>
+      </div>
+      <div class="settingsPanel-footer">
+        <button class="btn-flat">Done</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
