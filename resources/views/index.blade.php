d628b26e921605b0a2a113c3a6d6a6a2c5705ded
syropian <collinpatrickhenderson@gmail.com>
2016-08-18 18:48:43 -0400

    add GA snippet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,5 +16,18 @@
 <body>
 <div id="app"></div>
 <script src="{{ elixir("js/app.bundle.js") }}"></script>
+@if (App::environment('production'))
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53631521-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+@endif
+
 </body>
 </html>
