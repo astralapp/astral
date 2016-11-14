94f73b87f851b5364606e6000c7a75dcbe9ce75a
Alex Bowers <bowersbros@gmail.com>
2016-11-14 22:26:24 +0000

    Disable CSRF ** ATTENTION

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Astral\Http\Middleware\VerifyCsrfToken::class,
+//            \Astral\Http\Middleware\VerifyCsrfToken::class,
         ],
 
         'api' => [
