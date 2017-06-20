1f4a65d8b35ddf939166fef3de3eee491788e770
syropian <collinpatrickhenderson@gmail.com>
2017-06-19 20:05:19 -0400

    New favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <title>Astral</title>
-  <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="theme-color" content="#ffffff">
   <link href='//fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/latest/styles/github.min.css">
