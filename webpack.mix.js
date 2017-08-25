22e138adb157e24cbd5fd84cf6b4434f982d3974
syropian <collinpatrickhenderson@gmail.com>
2017-08-24 22:40:59 -0400

    Updates Vue version, fixes tag sorting dropdown styles, updates Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js/')
 .sass('resources/assets/sass/app.scss', 'public/css/');
 
-if (mix.config.inProduction) {
-    mix.version();
+if (process.env.NODE_ENV === 'production') {
+  mix.version();
 }
