93ad14ccadc18fea8534505a9d38bcf86faf8eea
Collin Henderson <collin@syropia.net>
2018-03-08 19:19:42 -0500

    Adds virtual scroller component to stars list for some big ol perf gains
    Also switches out SimpleMDE implementation for one that supports newer fontr awesome version.

--- resources/assets/js/components/Dashboard/Star.vue
+++ resources/assets/js/components/Dashboard/Star.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="p-4 border-b border-grey-light cursor-pointer hover:bg-grey-lightest transition-bg" :class="{'shadow-inner bg-grey-lightest': selected, 'bg-white': !selected}" @dragstart="starDragged">
+  <div class="p-4 border-b border-grey-light cursor-pointer hover:bg-grey-lightest transition-bg" :class="{'shadow-inner bg-grey-lightest': selected, 'bg-white': !selected}" @dragstart="starDragged">
     <h3 class="repo-name text-base text-brand mb-2 font-bold" v-once>{{ star.node.nameWithOwner }}</h3>
     <p class="text-dark-grey text-sm" v-once>{{ star.node.description }}</p>
     <star-tags 
@@ -24,7 +24,7 @@
         @click.stop>View on GitHub</a>
       </div>
     </div>
-  </li>
+  </div>
 </template>
 <script>
 import StarTags from '@/components/Dashboard/StarTags'
