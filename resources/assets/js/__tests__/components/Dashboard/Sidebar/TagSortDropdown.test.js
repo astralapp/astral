cc98abbe598578d8c20435767610356eefcd955f
Collin Henderson <collin@syropia.net>
2018-04-01 14:39:17 -0400

    Tag Sort Dropdown tests

---
+++ resources/assets/js/__tests__/components/Dashboard/Sidebar/TagSortDropdown.test.js
@@ -0,0 +1,48 @@
+import { shallow } from '@vue/test-utils'
+import TagSortDropdown from '@/components/Dashboard/Sidebar/TagSortDropdown'
+
+const $bus = {
+  $emit: jest.fn((e, data) => data)
+}
+
+describe('Tag Sort Dropdown', () => {
+  it('shows if visibility is true', () => {
+    const wrapper = shallow(TagSortDropdown, {
+      propsData: {
+        visible: true
+      },
+      mocks: { $bus }
+    })
+
+    expect(wrapper.find('.tag-sort-dropdown').isVisible()).toBe(true)
+  })
+  it('is hidden if visibility is false', () => {
+    const wrapper = shallow(TagSortDropdown, {
+      propsData: {
+        visible: false
+      },
+      mocks: { $bus }
+    })
+
+    expect(wrapper.find('.tag-sort-dropdown').isVisible()).toBe(false)
+  })
+  it('emits an event when a sort method is clicked', () => {
+    const wrapper = shallow(TagSortDropdown, {
+      propsData: {
+        visible: false
+      },
+      mocks: { $bus }
+    })
+
+    const busSpy = jest.spyOn(wrapper.vm.$bus, '$emit')
+
+    wrapper.findAll('.tag-sort-item').wrappers.forEach(item => {
+      item.trigger('click')
+    })
+
+    expect(busSpy).toHaveBeenCalledWith('TAGS_SORTED', 'ALPHA_ASC')
+    expect(busSpy).toHaveBeenCalledWith('TAGS_SORTED', 'ALPHA_DESC')
+    expect(busSpy).toHaveBeenCalledWith('TAGS_SORTED', 'STARS_DESC')
+    expect(busSpy).toHaveBeenCalledWith('TAGS_SORTED', 'STARS_ASC')
+  })
+})
