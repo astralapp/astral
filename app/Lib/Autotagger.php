f448644fb444bc7fdd90a9e8eecb23de67c0908b
Collin Henderson <collinpatrickhenderson@gmail.com>
2018-06-13 21:38:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Lib/Autotagger.php
+++ app/Lib/Autotagger.php
@@ -2,40 +2,42 @@
 
 namespace Astral\Lib;
 
-use Astral\Models\User;
 use Astral\Models\Star;
+use Astral\Models\User;
 
-class Autotagger {
-  protected $user;
+class Autotagger
+{
+    protected $user;
 
-  public function __construct(User $user)
-  {
-    $this->user = $user;
-  }
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
 
-  public function tagByTopic($stars) {
-    collect($stars['edges'])->each(function ($star) {
-        $starId = $star['node']['databaseId'];
-        $topics = collect($star['node']['repositoryTopics']['edges'])->map(function ($edge) {
-            return ['name' => $edge['node']['topic']['name']];
+    public function tagByTopic($stars)
+    {
+        collect($stars['edges'])->each(function ($star) {
+            $starId = $star['node']['databaseId'];
+            $topics = collect($star['node']['repositoryTopics']['edges'])->map(function ($edge) {
+                return ['name' => $edge['node']['topic']['name']];
+            });
+
+            if (count($topics)) {
+                $userStar = $this->user->stars()->withRepoId($starId)->first();
+
+                if (!$userStar) {
+                    $userStar = new Star();
+                    $userStar->repo_id = $starId;
+                    $userStar->user_id = $this->user->id;
+                    $userStar->save();
+                }
+
+                if (!$userStar->autotagged_by_topic) {
+                    $userStar->syncTags($topics, false);
+                    $userStar->autotagged_by_topic = true;
+                    $userStar->save();
+                }
+            }
         });
-
-        if (count($topics)) {
-            $userStar = $this->user->stars()->withRepoId($starId)->first();
-
-            if (!$userStar) {
-                $userStar = new Star();
-                $userStar->repo_id = $starId;
-                $userStar->user_id = $this->user->id;
-                $userStar->save();
-            }
-
-            if (!$userStar->autotagged_by_topic) {
-              $userStar->syncTags($topics, false);
-              $userStar->autotagged_by_topic = true;
-              $userStar->save();
-            }
-        }
-    });
-  }
+    }
 }
