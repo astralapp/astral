0fcdefc993cccfed14a43fd1fbc825bf80e89cee
Collin Henderson <syropian@users.noreply.github.com>
2019-07-30 11:26:10 -0400

    Apply fixes from StyleCI (#268)
    [ci skip] [skip ci]

--- database/factories/PredicateFactory.php
+++ database/factories/PredicateFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Predicate::class, function (Faker $faker) {
     return [
-        'name' => $faker->word(),
-        'body' => '{"key": "value"}',
+        'name'    => $faker->word(),
+        'body'    => '{"key": "value"}',
         'user_id' => function () {
             return factory('Astral\Models\User')->create()->id;
         },
