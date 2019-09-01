b708aa0c329d6b9668bd3562244085e9593e93a6
syropian <collinpatrickhenderson@gmail.com>
2019-09-01 13:54:55 -0400

    Adds migration to remove slug column from tags table

---
+++ database/migrations/2019_09_01_175344_drop_slug_column_from_tags.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropSlugColumnFromTags extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            //
+        });
+    }
+}
