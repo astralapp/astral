9ca22892c5fa90dbb016c985624b8c230d9e7c59
Collin Henderson <collinpatrickhenderson@gmail.com>
2025-01-02 15:25:14 -0500

    Fixes tag icon alignment in sidebar

--- resources/components/sidebar/SidebarItem.vue
+++ resources/components/sidebar/SidebarItem.vue
@@ -59,7 +59,7 @@ const Render = () =>
           'div',
           {
             ariaHidden: true,
-            class: `flex-shrink-0 w-5 h-5 mr-2 ${iconClasses.value}`,
+            class: `flex-shrink-0 w-4 h-4 mr-2 ${iconClasses.value}`,
           },
           slots.icon()
         ),
